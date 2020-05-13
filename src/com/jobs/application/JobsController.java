f95c7a7665a29ae4196281debf94c383c4360a18
fsarrias <fsarrias@kolab.es>
2020-05-13 10:16:52 +0200

    Subo el ejercicio con las correciones de jonatan

--- src/com/jobs/application/JobsController.java
+++ src/com/jobs/application/JobsController.java
@@ -3,6 +3,8 @@ package com.jobs.application;
 import com.jobs.domain.Employee;
 import com.jobs.domain.Volunteer;
 import com.jobs.persistence.EmployeeRepository;
+import com.jobs.domain.AbsStaffMember;
+import com.jobs.domain.Volunteer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,7 +28,7 @@ public class JobsController {
 	}
 
 	public void createManagerEmployee(String name, String address, String phone, double salaryPerMonth) throws Exception{
-		Employee manager = new Employee(name, address, phone,  salaryPerMonth, com.jobs.application.PaymentFactory.createPaymentRateEmployee());
+		Employee manager = new Employee(name, address, phone,  salaryPerMonth, com.jobs.application.PaymentFactory.createPaymentRateManager());
 		repository.addMember(manager);
 		// TODO Auto-generated method stub -  DONE!
 		
@@ -34,14 +36,15 @@ public class JobsController {
 
 
 	public void payAllEmployeers() {
-		List<com.jobs.domain.AbsStaffMember> miembros= new ArrayList<>();
+		List<AbsStaffMember> miembros= new ArrayList<>();
 		miembros = repository.getAllMembers();
-		for (com.jobs.domain.AbsStaffMember e : miembros){
-			if ( e instanceof Employee) {
-				((Employee)e).pay();
-			} else if ( e instanceof Volunteer) {
-				((Volunteer)e).pay();
-			}
+		for (AbsStaffMember e : miembros){
+			e.pay();
+			//if ( e instanceof Employee) {
+			//	((Employee)e).pay();
+			//} else if ( e instanceof Volunteer) {
+			//	((Volunteer)e).pay();
+			//}
 		}
 		// TODO Auto-generated method stub - DONE!
 	
@@ -49,9 +52,9 @@ public class JobsController {
 
 	public String getAllEmployees() {
 		String lista_miembros ="\n";
-		List<com.jobs.domain.AbsStaffMember> miembros= new ArrayList<>();
+		List<AbsStaffMember> miembros= new ArrayList<>();
 		miembros = repository.getAllMembers();
-		for (com.jobs.domain.AbsStaffMember e : miembros){
+		for (AbsStaffMember e : miembros){
 			//System.out.println( e.getName()+ " \n");
 			if ( e instanceof Employee) {
 				lista_miembros = lista_miembros + e.getName() + " ................Original Salary:" + ((Employee) e).getSalaryPerMonth() + " \n";
@@ -65,7 +68,7 @@ public class JobsController {
 	}
 
 	public void createVolunteer(String name, String address, String phone) throws Exception{
-		com.jobs.domain.Volunteer voluntario = new Volunteer(name, address, phone, "voluntario sin sueldo");
+		Volunteer voluntario = new Volunteer(name, address, phone, "voluntario sin sueldo");
 		repository.addMember(voluntario);
 		// TODO Auto-generated method stub - DONE!
 	}
--- src/com/jobs/application/PaymentFactory.java
+++ src/com/jobs/application/PaymentFactory.java
@@ -18,8 +18,21 @@ public class PaymentFactory {
 		return new IPaymentRate() {
 			@Override
 			public double pay(double salaryPerMonth) {
-				return 0;//todo 
+				return salaryPerMonth*0.85; //todo - DONE!
 			}
 		};
 	}
+
+	public static IPaymentRate createPaymentRateManager(){
+		return new IPaymentRate() {
+			@Override
+			public double pay(double salaryPerMonth) {
+				return salaryPerMonth*1.1; //todo - DONE!
+			}
+		};
+	}
+
+
+
+
 }
